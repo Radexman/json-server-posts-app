f61d568e83f0bf87628e758b35f4216b019ea29f
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-09-24 17:49:40 +0200

    Fetch All Posts

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const blogsEl = document.querySelector('.blogs');
+const uri = 'http://localhost:3000/posts';
+
+const renderPosts = async () => {
+	const response = await fetch(uri);
+	const data = await response.json();
+
+	let template = '';
+
+	data.forEach((post) => {
+		template += `
+            <div class="post">
+                <h2>${post.title}</h2>
+                <p>${post.likes} likes</p>
+                <p>${post.body.slice(0, 200)}</p>
+                <a href='/details.html'>read more... </a>
+            </div>
+        `;
+
+		blogsEl.innerHTML = template;
+	});
+};
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
